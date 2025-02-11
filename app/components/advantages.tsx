84c38db3b3ec4305a5b240c91a69fb8b8e798952
Ulephraim <iphraimunlimited@gmail.com>
2025-02-11 13:36:03 +0100

    Implement lucide react icons

--- app/components/advantages.tsx
+++ app/components/advantages.tsx
@@ -1,24 +1,25 @@
 /** @format */
 
 import advImage from '@/assets/svg/hero2.jpg';
-import globalizeImg from '@/assets/svg/globalize.svg';
+// import globalizeImg from '@/assets/svg/globalize.svg';
 import delightImg from '@/assets/svg/delight.svg';
 import enhanceImg from '@/assets/svg/enhance.svg';
-
 import Image from 'next/image';
+import { File, Globe, SmilePlus, TrendingDown } from 'lucide-react';
+import React from 'react';
 
 function Advantage({
   icon,
   title,
   content,
 }: {
-  icon: any;
+  icon: React.ReactNode;
   title: string;
   content: string;
 }) {
   return (
     <div className="advantage">
-      <Image src={icon} alt={title} />
+      <div className="adv-icon">{icon}</div>
       <article className="advantage-content">
         <h4 className="advantage-title">{title}</h4>
         <p className="advantage-text">{content}</p>
@@ -33,17 +34,17 @@ export default function Advantages() {
       <div className="main">
         <div className="adv-list">
           <Advantage
-            icon={globalizeImg}
+            icon={<Globe size={26} color="white" />}
             title="Expand Your Market Reach"
             content="Harness Datastral to take your product marketing global, enabling seamless collaboration and content distribution across multiple retail platforms."
           />
           <Advantage
-            icon={delightImg}
+            icon={<SmilePlus size={26} color="white" />}
             title="Enhance Customer Satisfaction"
             content="Use Datastral to refine your marketing approach, ensuring smooth collaboration and engaging content management that enhances customer satisfaction."
           />
           <Advantage
-            icon={enhanceImg}
+            icon={<TrendingDown size={26} color="white" />}
             title="Improve Cost Efficiency"
             content="Leverage Datastral to streamline operations, optimize spending, and enhance efficiency across your marketing processes."
           />
