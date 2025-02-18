740626616775cde896d4b2b26eecbc7235688460
Ulephraim <iphraimunlimited@gmail.com>
2025-02-18 23:15:25 +0100

    Implement Login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,65 @@
 /** @format */
 
 import React from 'react';
+import styles from '../../../styles/Login.module.scss';
+import datastralLogo from '../../../assets/svg/datastral.jpg';
+import Image from 'next/image';
 
-const page = () => {
-  return <div>Login</div>;
+const LoginPage: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <Image className={styles.logo} src={datastralLogo} alt="Datastral Logo" />
+      <div className={styles.loginBox}>
+        <h1 className={styles.title}>Sign in to your account</h1>
+        <form className={styles.form}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="email" className={styles.label}>
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className={styles.input}
+              required
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="password" className={styles.label}>
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className={styles.input}
+              required
+            />
+          </div>
+          <div className={styles.options}>
+            <a href="#" className={styles.link}>
+              Forgot your password?
+            </a>
+          </div>
+          <button type="submit" className={styles.submitButton}>
+            Continue
+          </button>
+        </form>
+        <div className={styles.socialLogin}>
+          <p>Or sign in with</p>
+          {/* social login buttons here */}
+        </div>
+        <div className={styles.signUp}>
+          <p>
+            Don’t have an account?{' '}
+            <a href="#" className={styles.link}>
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default LoginPage;
