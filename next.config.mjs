4850f3861d16c9db8fcc4ac7097957e6032fbcb4
Ulephraim <iphraimunlimited@gmail.com>
2025-03-14 10:26:00 +0100

    Updated next.config.mjs for configuration changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  metadataBase: new URL(
+    process.env.NODE_ENV === 'production'
+      ? 'https://datastral-template.vercel.app'
+      : 'http://localhost:3000'
+  ),
+};
 
 export default nextConfig;
